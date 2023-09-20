760d902a4136c94f79fec3586923d4effb5a770d
Gokul <gokul@saaragh.in>
2023-09-20 18:35:07 +0530

    Frontpage creation

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,11 +1,9 @@
-import Frontpage from "./Components/Frontpage";
 import Header from "./Components/Header";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Frontpage />
     </>
   );
 };
--- Frontend/src/Components/Header.tsx
+++ Frontend/src/Components/Header.tsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import { Company, Contact, Features, Home } from "./Frontpage";
+
 const Header = () => {
+  const [tab, setTab] = useState("Home");
+
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800 show-box">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a className="flex items-center">
               <img
@@ -17,15 +22,15 @@ const Header = () => {
             <div className="flex items-center lg:order-2">
               <a
                 href="#"
-                className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                className="text-gray-800 border dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
-                Log in
+                LOGIN
               </a>
               <a
                 href="#"
-                className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                className="text-gray-800 border dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
               >
-                Get started
+                SIGN UP
               </a>
               <button
                 data-collapse-toggle="mobile-menu-2"
@@ -68,15 +73,16 @@ const Header = () => {
               <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                 <li>
                   <a
+                    onClick={() => setTab("Home")}
                     href="#"
                     className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                    aria-current="page"
                   >
                     Home
                   </a>
                 </li>
                 <li>
                   <a
+                    onClick={() => setTab("Company")}
                     href="#"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
@@ -85,6 +91,7 @@ const Header = () => {
                 </li>
                 <li>
                   <a
+                    onClick={() => setTab("Features")}
                     href="#"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
@@ -93,6 +100,7 @@ const Header = () => {
                 </li>
                 <li>
                   <a
+                    onClick={() => setTab("Contact")}
                     href="#"
                     className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
@@ -104,6 +112,27 @@ const Header = () => {
           </div>
         </nav>
       </header>
+
+      {tab === "Home" && (
+        <div className="p-2 show-box">
+          <Home />
+        </div>
+      )}
+      {tab === "Company" && (
+        <div className="p-2 show-box">
+          <Company />
+        </div>
+      )}
+      {tab === "Features" && (
+        <div className="p-2 show-box">
+          <Features />
+        </div>
+      )}
+      {tab === "Contact" && (
+        <div className="p-2 show-box">
+          <Contact />
+        </div>
+      )}
     </>
   );
 };
