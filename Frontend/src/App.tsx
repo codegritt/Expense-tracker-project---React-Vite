cb35b99fbed96420875dd33efccc48a4367b2ee7
Gokul <gokul@saaragh.in>
2023-09-20 13:18:02 +0530

    Created header component

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import Frontpage from "./Components/Frontpage";
+import Header from "./Components/Header";
 
 const App = () => {
   return (
     <>
+      <Header />
       <Frontpage />
     </>
   );
