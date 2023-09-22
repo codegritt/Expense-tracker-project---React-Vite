a8269b72bb81468fae1d6cd8c417268e710b3d5c
Gokul <gokul@saaragh.in>
2023-09-22 16:15:11 +0530

    Dashboard area changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Loginform from "./Components/Loginform";
 import Loader from "./ReusableComponents/Loader/Loader";
-import styled from "styled-components";
 
 const App = () => {
   return (
@@ -17,9 +16,7 @@ const App = () => {
               path="/"
               element={
                 <RequireAuth>
-                  <AppStyled>
-                    <Header />
-                  </AppStyled>
+                  <Header />
                 </RequireAuth>
               }
             ></Route>
@@ -30,21 +27,5 @@ const App = () => {
     </>
   );
 };
-
-const AppStyled = styled.div`
-  height: 100vh;
-  position: relative;
-  main {
-    flex: 1;
-    background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #ffffff;
-    backdrop-filter: blur(4.5px);
-    border-radius: 32px;
-    overflow-x: hidden;
-    &::-webkit-scrollbar {
-      width: 0;
-    }
-  }
-`;
 
 export default App;
