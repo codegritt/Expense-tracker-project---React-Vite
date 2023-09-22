5072ed3303f80f47a3ae07b86a3e8008ac74daf2
Gokul <gokul@saaragh.in>
2023-09-22 18:39:15 +0530

    API integration

--- Frontend/src/Components/Expense.tsx
+++ Frontend/src/Components/Expense.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
 import ExpenseForm from "./ExpenseForm";
+import IncomeItem from "./IncomeItem";
+import { useGlobalContext } from "../context/globalContext";
 
 const Expense = () => {
+  const { addIncome, expenses, getExpenses, deleteExpense, totalExpenses } =
+    useGlobalContext();
+
+  useEffect(() => {
+    getExpenses();
+  }, []);
   return (
     <>
       <div>
@@ -9,6 +18,27 @@ const Expense = () => {
         </h1>
         <div className="mt-5">
           <ExpenseForm />
+        </div>
+        <div className="incomes">
+          {expenses.map((income) => {
+            const { _id, title, amount, date, category, description, type } =
+              income;
+            console.log(income);
+            return (
+              <IncomeItem
+                key={_id}
+                id={_id}
+                title={title}
+                description={description}
+                amount={amount}
+                date={date}
+                type={type}
+                category={category}
+                indicatorColor="var(--color-green)"
+                deleteItem={deleteExpense}
+              />
+            );
+          })}
         </div>
       </div>
     </>
--- Frontend/src/Components/Income.tsx
+++ Frontend/src/Components/Income.tsx
@@ -1,5 +1,6 @@
 import { useGlobalContext } from "../context/globalContext";
 import IncomeForm from "./IncomeForm";
+import IncomeItem from "./IncomeItem";
 
 const Income = () => {
   const { addIncome, incomes, getIncomes, deleteIncome, totalIncome } =
@@ -12,6 +13,23 @@ const Income = () => {
       <div className="mt-5">
         <IncomeForm />
       </div>
+      <div className="incomes">
+                        {incomes.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                                deleteItem={deleteIncome}
+                            />
+                        })}
+                    </div>
     </div>
   );
 };
